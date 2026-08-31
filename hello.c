439de95753e8476ebf094ba8639795d922440069
Annabel Patschke <apatschke2@huskers.unl.edu>
2026-08-31 16:29:05 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Annabel Patschke
+ *         apatschke2@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World\n");
+
+  return 0;
+}
+
